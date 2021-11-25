03f9f212a9452f48ce00f55ba1596184c363e273
Mindohyeon <81687906+Mindohyeon@users.noreply.github.com>
2021-11-25 15:06:21 +0900

    객체 생성 , list 에 item 추가

--- app/src/main/java/com/example/downbitjava/MainActivity.java
+++ app/src/main/java/com/example/downbitjava/MainActivity.java
@@ -11,8 +11,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.downbitjava.databinding.ActivityMainBinding;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -41,6 +44,16 @@ public class MainActivity<mTimer> extends AppCompatActivity implements View.OnCl
     int ed_change;
     int ripple = price();
     int ripple_change;
+    int hive = price();
+    int hive_change;
+    int sand = price();
+    int sand_change;
+    int bora = price();
+    int bora_change;
+    int tron = price();
+    int tron_change;
+    int icon = price();
+    int icon_change;
     double upping = 0;
     int tmp = 0;
 
@@ -50,10 +63,44 @@ public class MainActivity<mTimer> extends AppCompatActivity implements View.OnCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ProfileData coin1 = new ProfileData("비트코인", bitcoin_change, upping, bitcoin);
-        calculate(coin1);
+        profileDataArrayList = new ArrayList<>();
 
+        ProfileData coin1 = new ProfileData("비트코인", bitcoin_change, upping, bitcoin);
         ProfileData coin2 = new ProfileData("도지코인", doz_change, upping, doz);
+        ProfileData coin3 = new ProfileData("이더리움", ed_change, upping, ed);
+        ProfileData coin4 = new ProfileData("리플", ripple_change, upping, ripple);
+        ProfileData coin5 = new ProfileData("하이브", hive_change,upping,hive);
+        ProfileData coin6 = new ProfileData("샌드박스", sand_change,upping,sand);
+        ProfileData coin7 = new ProfileData("보라",bora_change,upping,bora);
+        ProfileData coin8 = new ProfileData("트론",tron_change,upping,tron);
+        ProfileData coin9 = new ProfileData("아이콘",icon_change,upping,icon);
+
+
+
+
+        upping = random.nextInt(200);
+//
+//        tmp = bitcoin;
+//        bitcoin *= upping;
+//        bitcoin_change = bitcoin - tmp;
+        calculate(coin1);
+        profileDataArrayList.add(coin1);
+        calculate(coin2);
+        profileDataArrayList.add(coin2);
+        calculate(coin3);
+        profileDataArrayList.add(coin3);
+        calculate(coin4);
+        profileDataArrayList.add(coin4);
+        calculate(coin5);
+        profileDataArrayList.add(coin5);
+        calculate(coin6);
+        profileDataArrayList.add(coin6);
+        calculate(coin7);
+        profileDataArrayList.add(coin7);
+        calculate(coin8);
+        profileDataArrayList.add(coin8);
+        calculate(coin9);
+        profileDataArrayList.add(coin9);
 
 
         swipeRefreshLayout = findViewById(R.id.swiperefresh);
@@ -71,21 +118,7 @@ public class MainActivity<mTimer> extends AppCompatActivity implements View.OnCl
         View property_btn = findViewById(R.id.property_btn);
         property_btn.setOnClickListener(this);
 
-
         mRecyclerView = findViewById(R.id.recyclerviewid);
-
-        profileDataArrayList = new ArrayList<>();
-
-
-        profileDataArrayList.add(coin1);
-        profileDataArrayList.add(coin2);
-
-
-        profileDataArrayList.add(new ProfileData("이더리움", ed_change, upping, ed));
-
-
-        profileDataArrayList.add(new ProfileData("리플", ripple_change, upping, ripple));
-
 
         //Adapter 시작 (initiate adapter)
         myRecyclerViewAdapter = new RecyclerViewAdapter();
@@ -104,7 +137,7 @@ public class MainActivity<mTimer> extends AppCompatActivity implements View.OnCl
     }
 
     void calculate(ProfileData a) {
-        upping = random.nextInt(200);
+        a.upping = random.nextInt(200);
 
         tmp = a.price;
         a.price *= upping;
